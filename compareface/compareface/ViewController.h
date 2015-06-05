cc653a844b8901bf93770d34bce889cd33d25b94
xiaomochn <991819166@qq.com>
2015-06-05 16:02:24 +0800

    11

---
+++ compareface/compareface/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  compareface
+//
+//  Created by qiao on 15/6/5.
+//  Copyright (c) 2015年 qiao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
